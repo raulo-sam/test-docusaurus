e90961b22d9b31f7e7425765caba4c216b0c64d6
Raul <rauldiazlarraga@gmail.com>
2023-02-19 20:39:32 +0100

    second commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,13 +15,13 @@ const config = {
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/raulo-sam/test-docusaurus/',
+    baseUrl: '/raulo-sam/test-docusaurus/',
+    deploymentBranch: 'gh-pages',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'raulo-sam', // Usually your GitHub org/user name.
+  projectName: 'test-docusaurus', // Usually your repo name.
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
